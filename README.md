cc227584814f02a2e0ddc7d6af45bbb88ea5e977
Killian Poulaud <killian.1024@gmail.com>
2018-08-06 21:17:39 +0200

    1.0.0

---
+++ README.md
@@ -0,0 +1,19 @@
+### metamorphosis ###
+
+metamorphosis is a C++ program whose propose is rename a set of files following a specified patern. 
+For more information use `./metamorphosis --help` command.
+
+### Build ###
+
+Use the folowing commands to buil the CMake project.
+
+    cmake .
+    cmake --build .
+
+#### Dependencies ####
+
+In order to compile this software you have to use a C++ revision equal or highter to C++17 
+(ISO/IEC 14882:2017). Also the following libraries are necessary:
+- speed (https://github.com/killian1024/speed)
+- libjpeg
+- libpng
---
+++ src/file_target.cpp
@@ -0,0 +1,126 @@
+/* metamorphosis
+ * Copyright (C) 2018 Killian Poulaud.
+ *
+ * This file is part of metamorphosis.
+ *
+ * metamorphosis is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * metamorphosis is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with speed. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//
+// Created by Killian on 06/08/18.
+//
+
+#include <speed/speed.hpp>
+
+#include "file_target.hpp"
+
+
+namespace metamorphosis {
+
+
+file_target::file_target(std::filesystem::path fle_pth, std::vector<file_target*>* fles_target)
+        : fle_pth_(std::move(fle_pth))
+        , new_fle_nme_()
+        , dne_(false)
+        , visitd_(false)
+        , id_(nxt_id_++)
+        , fles_target_(fles_target)
+{
+}
+
+
+int file_target::rename() noexcept
+{
+    std::filesystem::path new_fle_pth = fle_pth_;
+    int res;
+    
+    if (dne_)
+    {
+        return 0;
+    }
+    else if (visitd_)
+    {
+        std::string new_fle_nme = "metamorphosis-tmp-file-name-";
+        new_fle_nme += std::to_string(id_);
+        new_fle_pth.replace_filename(new_fle_nme);
+    }
+    else
+    {
+        new_fle_pth.replace_filename(new_fle_nme_);
+        new_fle_pth += fle_pth_.extension();
+    
+        visitd_ = true;
+    
+        if (fle_pth_ !=  new_fle_pth &&
+            std::filesystem::exists(new_fle_pth))
+        {
+            for (auto& x : *fles_target_)
+            {
+                if (x->fle_pth_ == new_fle_pth)
+                {
+                    if (x->rename() != 0)
+                    {
+                        dne_ = true;
+                        return -1;
+                    }
+                    
+                    break;
+                }
+            }
+        }
+        
+        dne_ = true;
+    }
+    
+    std::cout << spdios::set_light_blue_text << "Renaming "
+              << spdios::set_yellow_text << fle_pth_.filename()
+              << spdios::set_light_blue_text << " --> "
+              << spdios::set_yellow_text << new_fle_pth.filename()
+              << spdios::set_light_blue_text << " ...";
+              
+    if ((res = ::rename(fle_pth_.c_str(), new_fle_pth.c_str())) == 0)
+    {
+        fle_pth_ = new_fle_pth;
+        
+        std::cout << spdios::set_light_green_text << "[ok]" << spdios::newl;
+    }
+    else
+    {
+        std::cout << spdios::set_light_red_text << "[fail]" << spdios::newl;
+    }
+    
+    return res;
+}
+
+
+bool sort_alphabeticaly::operator()(const file_target* rhs, const file_target* lhs)
+{
+    if (rhs->get_file_path().empty())
+    {
+        return !lhs->get_file_path().empty();
+    }
+    else if (lhs->get_file_path().empty())
+    {
+        return false;
+    }
+    
+    return rhs->get_file_path().filename().string().compare(
+            lhs->get_file_path().filename().string()) < 0;
+}
+
+
+std::size_t file_target::nxt_id_ = 0;
+
+
+}
